877704014eff361a7a1feae2ba2e64ae69a427f2
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-03-13 08:00:38 +0000

    [Update]customer/sign_up,sign_in, flash_messages

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -42,7 +42,15 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :postal_code,
+      :address,
+      :telephone_number
+    ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
@@ -52,7 +60,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    mypage_path
+    root_path
   end
 
   def after_sign_out_path_for(resource)
