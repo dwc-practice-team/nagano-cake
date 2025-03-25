daf4b37ed7830242537ce44e5e2e29fa63e2daf5
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-03-25 14:26:49 +0000

    [Edit, Fix, Create]layouts/admin -> header, routes -> itmes=>items, genres/index.html.erb&genres_controller/index&create

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,22 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
+  layout 'admin'
+
   def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "ジャンルの新規登録が完了しました"
+      redirect_to admin_genres_path
+    else
+      flash.now[:danger] = "ジャンルの登録に失敗しました"
+      @genres = Genre.all
+      render :index
+    end
   end
 
   def edit
