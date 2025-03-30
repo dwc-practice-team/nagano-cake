9526854aff2d7908e287d79a6cfac9eea30a2979
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-03-30 14:18:42 +0000

    [Update]index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
+  layout 'admin'
+  
   def index
     @items = Item.page(params[:page])
   end
@@ -8,9 +10,26 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "新規登録が完了しました"
+      redirect_to admin_item_path(@item)
+    else
+      flash.now[:danger] = "新規登録に失敗しました"
+      render :new
+    end
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :genre_id, :price, :is_active)
   end
 end
