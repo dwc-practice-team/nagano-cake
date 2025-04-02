faae5aa6e6468a67d44dfecda23275e91188a264
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-04-02 07:06:38 +0000

    [Update]controller,view(index)

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page]).per(8)
+    @all_items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
