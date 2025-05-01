2205f48191c87031609ac0f5ed1227038e6a4180
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-05-01 16:51:07 +0000

    [Update]orders/confirm, thanks

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details
 
   validates :name, presence: true
   validates :introduction, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :is_active, presence: true
   validates :genre_id, presence: true
 
